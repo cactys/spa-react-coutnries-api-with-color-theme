c07095ad8dbb6450675d8d43a842dbe25cbd8504
cactys <cactys95@yandex.ru>
2023-07-27 15:56:42 +0300

    fix again

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -41,8 +41,7 @@ const App = () => {
         })
         .catch((err) => console.error(err));
     }
-    // eslint-disable-next-line
-  }, []);
+  }, [countries]);
 
   useEffect(() => {
     handleSearch();
